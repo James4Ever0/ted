813bd3c035c4ade94d03369dc7a59513fe8c0743
Your Name <you@example.com>
2025-07-15 23:13:51 +0800

    modify setup.py for uploading

--- setup.py
+++ setup.py
@@ -1,4 +1,46 @@
-from setuptools import setup, find_packages
+from setuptools import setup, find_packages, Command
+import os
+from shutil import rmtree
+import sys
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+class UploadCommand(Command):
+    """Support setup.py upload."""
+
+    description = 'Build and publish the package.'
+    user_options = []
+
+    @staticmethod
+    def status(s):
+        """Prints things in bold."""
+        print('\033[1m{0}\033[0m'.format(s))
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        try:
+            self.status('Removing previous builds…')
+            rmtree(os.path.join(here, 'dist'))
+            rmtree(os.path.join(here, 'build'))
+        except OSError:
+            pass
+
+        self.status('Building Source and Wheel (universal) distribution…')
+        os.system('{0} setup.py sdist bdist_wheel --universal'.format(sys.executable))
+
+        self.status('Uploading the package to PyPI via Twine…')
+        os.system('twine upload dist/*')
+
+        # self.status('Pushing git tags…')
+        # os.system('git tag v{0}'.format(about['__version__']))
+        # os.system('git push --tags')
+
+        sys.exit()
 
 
 setup(
@@ -28,5 +70,8 @@ setup(
     include_package_data=True,
     package_data={
         'ted': ['*.css'],
+    },
+    cmdclass={
+        'upload': UploadCommand,
     },
 )
